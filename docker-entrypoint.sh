4fb31a07a1a01f012a332bf5c6d4aab81a7e1064
pierreozoux <pierre@ozoux.net>
2015-12-07 19:17:54 +0000

    fix data error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 if [ ! -e '/var/www/html/README.md' ]; then
   tar cf - --one-file-system -C /usr/src/rs . | tar xf -
-  mkdir ./data
   chown -R www-data:www-data /var/www/html
   chmod -R 750 /var/www/html
   chmod -R 750 /var/www/html/data
