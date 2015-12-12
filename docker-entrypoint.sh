6d53712b642a157cadfff12225388612d21109fd
Pierre Ozoux <pierre@ozoux.net>
2015-12-12 19:41:03 +0000

    codestyle

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-  tar cf - --one-file-system -C /usr/src/rs . | tar xf -
-  chown -R www-data:www-data /var/www/html
-  chmod -R 750 /var/www/html
-  chmod -R 750 /var/www/html/data
+tar cf - --one-file-system -C /usr/src/rs . | tar xf -
+chown -R www-data:www-data /var/www/html
+chmod -R 750 /var/www/html
+
 if [ ! -e './config/server.yaml' ]; then
   mv ./config/server.yaml.example ./config/server.yaml
 fi
